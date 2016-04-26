06497c573415324299433ffa7374717f07ec257c
Jarek Przygódzki <jarek.przygodzki@gmail.com>
2016-04-26 21:05:16 +0200

    Simple WinAPI app with window

--- winapi_opengl.d
+++ winapi_opengl.d
@@ -5,6 +5,8 @@ import core.sys.windows.windows;
 
 import std.utf;
 
+pragma(lib, "gdi32.lib");
+
 auto toUTF16z(S)(S s) {
     return toUTFz!(const(wchar)*)(s);
 }
@@ -28,6 +30,73 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int i
 }
 
 int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow) {
-    MessageBox(NULL, "Hello, Windows!", "Your Application", 0);
-    return 0;
+    string appName = "WindowsOpenGlApp";
+    HWND hwnd;
+    MSG  msg;
+    WNDCLASSEX wndclass;
+
+    wndclass.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndclass.lpfnWndProc   = &WndProc;
+    wndclass.cbClsExtra    = 0;
+    wndclass.cbWndExtra    = 0;
+    wndclass.hInstance     = hInstance;
+    wndclass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = cast(HBRUSH) GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName  = NULL;
+    wndclass.lpszClassName = appName.toUTF16z;
+
+    if(!RegisterClassEx(&wndclass)) {
+        MessageBox(NULL, "This program requires Windows NT!", appName.toUTF16z, MB_ICONERROR);
+        return 0;
+    }
+
+    hwnd = CreateWindow(appName.toUTF16z,      // window class name
+                         "Windows Program",    // window caption
+                         WS_OVERLAPPEDWINDOW,  // window style
+                         CW_USEDEFAULT,        // initial x position
+                         CW_USEDEFAULT,        // initial y position
+                         CW_USEDEFAULT,        // initial x size
+                         CW_USEDEFAULT,        // initial y size
+                         NULL,                 // parent window handle
+                         NULL,                 // window menu handle
+                         hInstance,            // program instance handle
+                         NULL);                // creation parameters
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+extern(Windows)
+LRESULT WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) nothrow {
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message) {
+        case WM_CREATE:
+            return 0;
+
+        case WM_PAINT:
+            hdc = BeginPaint(hwnd, &ps);
+            scope(exit) EndPaint(hwnd, &ps);
+
+            GetClientRect(hwnd, &rect);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
